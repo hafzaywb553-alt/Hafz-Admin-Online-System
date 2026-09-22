307927867b9243f3cec2e9b76738346134e1520d
hafzaywb553-alt <hafzaywb553@gmail.com>
2026-09-23 01:05:50 +0430

    Restore runtime file to pre-i18n-change stable version

--- settings.js
+++ settings.js
@@ -1779,11 +1779,6 @@ function applySystemName(settings) {
 }
 
 
-if (typeof window !== "undefined" && window.KrhaI18n?.registerDictionary) {
-    window.KrhaI18n.registerDictionary(TRANSLATIONS);
-}
-
-
 /* =========================================================
    Translation
 ========================================================= */
